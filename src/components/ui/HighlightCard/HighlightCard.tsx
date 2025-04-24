f56a24924ad87656a97c889ae088d49014d42293
cheporte <cheporte.dev@gmail.com>
2025-04-24 13:35:27 +0300

    🌟 Feat: Updated language selection section to change destination route

--- src/components/ui/HighlightCard/HighlightCard.tsx
+++ src/components/ui/HighlightCard/HighlightCard.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './HighlightCard.sass';
 
 type CardProps = {
     title: string;
     image?: string;
-    description?: string
+    description?: string;
+    onclick?: () => void;
 }
 
 const HighlightCard: React.FC<CardProps> = ({
     title,
     image,
-    description
+    description,
+    onclick
 }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+        setTimeout(() => setIsClicked(false), 150)
+        onclick?.();
+    }
+
     return (
-        <div className='card-container'>
+        <div 
+            className={`card-container ${isClicked ? 'card-container__clicked' : ''}`}
+            onClick={handleClick}
+        >
             {image && <img src={image} alt={title} />}
             <h2 className='card-container__title'>{title}</h2>
             {description && <p>{description}</p>}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../styles/pages/home.sass';
 
@@ -7,25 +7,33 @@ import Navcard from "../components/ui/Navcard/Navcard";
 import HighlightCard from "../components/ui/HighlightCard";
 
 const Home: React.FC = () => {
+    const [selectedLang, setSelectedLang] = useState('')
+    
     return (
         <div id='home-page'>
             <Hero />
             <section className='lang-highlight'>
                 <h2 className="section-title">Discover New Horizons</h2>
                 <div className="lang-highlight__options">
-                    <HighlightCard title="French" description="When you feel romantic" />
-                    <HighlightCard title="Japanese" description="Something cute? UwU" />
-                    <HighlightCard title="Ukrainian" description="Get what nightingales sing about" />
+                    <div onClick={() => {setSelectedLang('fr')}}>
+                        <HighlightCard title="French" description="When you feel romantic" />
+                    </div>
+                    <div onClick={() => {setSelectedLang('jp')}}>
+                        <HighlightCard title="Japanese" description="Something cute? UwU" />
+                    </div>
+                    <div onClick={() => {setSelectedLang('ua')}}>
+                        <HighlightCard title="Ukrainian" description="Get what nightingales sing about" />
+                    </div>
                 </div>
             </section>
             
             <section className='home-page__navigation'>
                 <h2 className="section-title">Choose Your Way Of Studying</h2>
                 <div className='home-page__navigation-options'>
-                    <Navcard title="Extend Dictionary" redirectTo="/dict"/>
-                    <Navcard title="Master Flashcards" redirectTo="/flashcards"/>
-                    <Navcard title="Read Texts" redirectTo="/reading"/>
-                    <Navcard title="Watch & Listen" redirectTo="/media"/>
+                    <Navcard title="Extend Dictionary" redirectTo={`/dict/${selectedLang}`}/>
+                    <Navcard title="Master Flashcards" redirectTo={`/flashcards/${selectedLang}`}/>
+                    <Navcard title="Read Texts" redirectTo={`/reading/${selectedLang}`}/>
+                    <Navcard title="Watch & Listen" redirectTo={`/media/${selectedLang}`}/>
                 </div>
             </section>
         </div>
