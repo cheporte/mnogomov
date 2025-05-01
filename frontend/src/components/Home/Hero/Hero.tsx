49395c09d0b27ad1135e9c8d909dba8cba272bc4
cheporte <cheporte.dev@gmail.com>
2025-05-01 19:24:45 +0300

    🩹 Fix: Fixed path to public folder in Hero.tsx

--- frontend/src/components/Home/Hero/Hero.tsx
+++ frontend/src/components/Home/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import Button from "../../ui/Button";
 const Hero: React.FC = () => {
     return (
         <div className='hero'>
-            <img className="hero__logo" src="/mnogomov-night.svg" alt="Site Logo" />
+            <img className="hero__logo" src="/mnogomov/mnogomov-night.svg" alt="Site Logo" />
             <div className="hero__content">
                 <h1 className="hero__content__title">Unlock the World with Mnogomov!</h1>
                 <p className="hero__content__subtitle">Dive into a new language and explore endless possibilities. Your journey to fluency starts here!</p>
